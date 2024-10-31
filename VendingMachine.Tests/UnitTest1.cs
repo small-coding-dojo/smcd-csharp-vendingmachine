729dece0e94b0d4c8c5bd4231c3d778cac07b58f
Niklas Rueber <nrueber@igus.net>
2024-10-31 16:46:33 +0100

    feat: vending machine accepts 1 nickel or 1 dime
    Co-authored-by: Stefan Boos <kontakt@boos.systems>

--- VendingMachine.Tests/UnitTest1.cs
+++ VendingMachine.Tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace VendingMachine.Tests;
 
 public class UnitTest1
@@ -5,6 +7,64 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
-        Assert.True(true);
+        var display = new TestableDisplay();
+        var sut = new VendingMachine(display); 
+        sut.InsertCoin(NewCoins.Nickel);
+        Assert.Equal("Balance: $0,05", display.Output);
     }
+    
+    [Fact]
+    public void Test2()
+    {
+        var display = new TestableDisplay();
+        var sut = new VendingMachine(display); 
+        sut.InsertCoin(NewCoins.Dime);
+        Assert.Equal("Balance: $0,10", display.Output);
+    }
+}
+
+public interface ICanDisplay
+{
+    void Update(string message);
+}
+
+public class TestableDisplay : ICanDisplay
+{
+    public string Output { get; set; }
+    
+    public void Update(string message)
+    {
+        Output = message;
+    }
+}
+
+public class VendingMachine(ICanDisplay display)
+{
+    public void InsertCoin(Coin coin)
+    {
+        var amount = DetermineValue(coin);
+        display.Update($"Balance: ${amount:F2}");
+    }
+
+    private static double DetermineValue(Coin coin)
+    {
+        switch (coin.Weight)
+        {
+            case "5 g":
+                return 0.05;
+            
+            case "10 g":
+                return 0.10;
+            default:
+                throw new ArgumentException($"Invalid weight of {coin.Weight}");
+        }
+    }
+}
+
+public record struct Coin(string Weight);
+
+public class NewCoins
+{
+    public static Coin Nickel = new Coin("5 g");
+    public static Coin Dime = new Coin("10 g");
 }
